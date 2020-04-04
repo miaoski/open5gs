6f520b786104774dd0a7747ecfc27ff0babf503c
Sukchan Lee <acetcom@gmail.com>
2020-04-04 18:22:18 -0400

    merge master

--- src/smf/s5c-build.c
+++ src/smf/s5c-build.c
@@ -385,6 +385,7 @@ static int16_t smf_pco_build(uint8_t *pco_buf, ogs_gtp_tlv_pco_t *tlv_pco)
     ogs_ipsubnet_t p_cscf, p_cscf6;
     int size = 0;
     int i = 0;
+    uint16_t mtu = 0;
 
     ogs_assert(pco_buf);
     ogs_assert(tlv_pco);
@@ -518,7 +519,7 @@ static int16_t smf_pco_build(uint8_t *pco_buf, ogs_gtp_tlv_pco_t *tlv_pco)
             break;
         case OGS_PCO_ID_IPV4_LINK_MTU_REQUEST:
             if (smf_self()->mtu) {
-                uint16_t mtu = htons(smf_self()->mtu);
+                mtu = htons(smf_self()->mtu);
                 smf.ids[smf.num_of_id].id = ue.ids[i].id;
                 smf.ids[smf.num_of_id].len = sizeof(uint16_t);
                 smf.ids[smf.num_of_id].data = &mtu;
